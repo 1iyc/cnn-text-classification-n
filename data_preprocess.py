bd834698d11d6e414367433f0a6c084de4a35c33
yiyc-kor <yiyc.kor@gmail.com>
2019-03-14 17:44:18 +0900

    feat: Delete category mode

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,21 +1,20 @@
 import numpy as np
 import re
 
-def load_data(data_file, class_file, char, category_level):
+
+def load_data(data_file, class_file, char):
     if char:
         data_examples = list(open(data_file, "r", encoding='utf-8').readlines())
         data_examples = [re.sub(r"\s{2,}", " <SP> ", re.sub(r"", " ", s.strip())).strip() for s in data_examples]
         print("Data Read & Transformed to Char Finished!")
-
-        class_examples = list(open(class_file, "r", encoding='utf-8').readlines())
-    if category_level:
-        class_examples = [re.sub(r" ", "", s.strip())[:category_level*2] for s in class_examples]
-        print("Code Read & Transformed to Class Finished!")
     else:
-        class_examples = [re.sub(r" ", "", s.strip()) for s in class_examples]
-        print("Code Read & Transformed to Class Finished!")
+        data_examples = list(x.strip() for x in open(data_file, "r", encoding='utf-8').readlines())
+        print("Data Read & Transformed to Word Finished!")
+
+    class_examples = list(x.strip() for x in open(class_file, "r", encoding='utf-8').readlines())
 
     return [data_examples, class_examples]
+
 
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
     """
@@ -35,6 +34,7 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
             start_index = batch_num * batch_size
             end_index = min((batch_num + 1) * batch_size, data_size)
             yield shuffled_data[start_index:end_index]
+
 
 def del_space(raw_list):
     raw_list = [re.sub(r" ", "", raw) for raw in raw_list]
