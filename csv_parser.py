ed02e79a677c0d4945ccd4474cabef9bcf9f6e9b
yiyc-kr <yiyc.kr@gmail.com>
2020-02-17 19:28:03 +0900

    Update csv_parser.py

--- csv_parser.py
+++ csv_parser.py
@@ -1,27 +1,50 @@
 #! /usr/bin/env python
 
 import argparse
+import csv
 
 parser = argparse.ArgumentParser(description="Refining Data & Class File before clustering")
 
 # input file
 parser.add_argument('--data_file', dest="data_file", type=str, default="./data/data.txt", help="Data File Path")
 
-#
+# extract evaluate rate
+parser.add_argument('--eval_rate', dest="eval_rate", type=float, default=None, help="evaluation rate")
 
 args = parser.parse_args()
 
-def csv_parse(data_file):
-    with open(data_file, 'r', encoding="utf-8") as f:
-        with open(data_file + "_csv_parsed_data.txt", 'w', encoding="utf-8") as g:
-            with open(data_file + "_csv_parsed_class.txt", 'w', encoding="utf-8") as h:
-                for line in f.readlines():
-                    parsed_line_array = line.split(",")
-                    g.write("".join(parsed_line_array[1:-2]) + "\n")
-                    h.write(parsed_line_array[-2:][0] + "\n")
+
+def csv_parse(data_file, eval_rate):
+    if eval_rate:
+        import random
+        total_count = len(open(data_file).readlines())
+        eval_count = int(total_count * eval_rate)
+        eval_list = sorted(random.sample(range(total_count), eval_count))
+    with open(data_file, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        # skip first line
+        next(csv_reader)
+        count = 1
+        for lines in csv_reader:
+            if count in eval_list:
+                with open("./eval_class_" + data_file.split("/")[-1][:-4] + ".txt", 'a', encoding="utf-8") as f:
+                    f.write(lines[0].strip() + "\n")
+                with open("./eval_name_" + data_file.split("/")[-1][:-4] + ".txt", 'a', encoding="utf-8") as f:
+                    f.write("".join(lines[1:]).strip() + "\n")
+                count += 1
+                continue
+            else:
+                with open("./train_class_" + data_file.split("/")[-1][:-4] + ".txt", 'a', encoding="utf-8") as f:
+                    f.write(lines[0].strip() + "\n")
+                with open("./train_name_" + data_file.split("/")[-1][:-4] + ".txt", 'a', encoding="utf-8") as f:
+                    f.write("".join(lines[1:]).strip() + "\n")
+                count += 1
+                continue
+
 
 def main():
-    csv_parse(args.data_file)
+    csv_parse(args.data_file, args.eval_rate)
+
 
 if __name__ == '__main__':
     main()
